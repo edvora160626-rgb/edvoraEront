989cabaf634c9b77e02c036e1ccba55a87333c63
edvora160626-rgb <edvora160626@gmail.com>
2026-07-11 21:12:01 +0530

    LISTCACHE Changes

--- src/utils/requestsApi.js
+++ src/utils/requestsApi.js
@@ -7,7 +7,9 @@ import {
   isFetchableRole,
 } from "./rolePermissions";
 
-const API_BASE = "http://localhost:4001";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
+const LIST_CACHE_TTL_MS = 30_000; // 30 seconds
 
 export const REQUEST_STATUSES = [
   { id: "REQUESTED", label: "Pending" },
